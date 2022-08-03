b1ffe06afb762781dac514999c2b186de2ba0a58
juttavirta <jutta563@gmail.com>
2022-08-03 19:42:25 +0300

    added TopicClass

---
+++ DiaryMVC/Models/TopicClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiaryMVC.Models
+{
+    public class TopicClass
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? TimeToMaster { get; set; }
+        public int? TimeSpent { get; set; }
+        public string Source { get; set; }
+        public DateTime? StartLearningDate { get; set; }
+        public bool? InProgress { get; set; }
+    }
+}
